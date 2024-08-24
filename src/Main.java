5a3550c84ac0f061678fd4b3720755561b43ad5d
inuree <hagnew59@gmail.com>
2024-08-24 18:18:44 -0400

    A few changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
+class Vehicle {
+    private String color;
+    private String make;
+    private String model;
+    private int year;
+
+    public Vehicle(String color, String make, String model, int year) {
+        this.color = color;
+        this.make = make;
+        this.model = model;
+        if(year>1850 && year<2025) {
+            this.year = year;
+        }
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setMake(String make) {
+        this.make = make;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public String getColor() {
+        return color;
+    }
+    public String getMake() {
+        return make;
+    }
+    public String getModel() {
+        return model;
+    }
+    public int getYear() {
+        return year;
+    }
+}
+
+class Dealership {
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
     }
 }
