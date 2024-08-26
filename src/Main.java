3980eaa01028cfd401d0740bab1673199142e4d7
inuree <hagnew59@gmail.com>
2024-08-25 23:28:45 -0400

    A few changes

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,57 @@
-import java.util.Scanner;
-
-class Vehicle {
-    private String color;
-    private String make;
-    private String model;
-    private int year;
-
-    public Vehicle(String color, String make, String model, int year) {
-        this.color = color;
-        this.make = make;
-        this.model = model;
-        if(year>1850 && year<2025) {
-            this.year = year;
+class Driver {
+    public static void updateFrequencies(Symbol[] symbols) {
+        int totalUses = 0;
+        for (Symbol symbol : symbols) {
+            totalUses += symbol.uses;
+        }
+        for (Symbol symbol : symbols) {
+            symbol.frequency = (double) symbol.uses / totalUses;
         }
     }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-    public void setMake(String make) {
-        this.make = make;
-    }
-    public void setModel(String model) {
-        this.model = model;
-    }
-    public void setYear(int year) {
-        this.year = year;
-    }
-    public String getColor() {
-        return color;
-    }
-    public String getMake() {
-        return make;
-    }
-    public String getModel() {
-        return model;
-    }
-    public int getYear() {
-        return year;
+    public static void sortSymbols(Symbol[] symbols) {
+        Symbol temp;
+        int arrLength = symbols.length;
+        boolean swapped;
+        for (int i = 0; i < arrLength - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < arrLength - i - 1; j++) {
+                if (symbols[j].frequency > symbols[j + 1].frequency) {
+                    temp = symbols[j];
+                    symbols[j] = symbols[j + 1];
+                    symbols[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
     }
 }
 
-class Dealership {
+class Symbol {
+    public char symbol;
+    public int uses;
+    public double frequency;
 
+    public Symbol(char symbol) {
+        this.symbol = symbol;
+        this.uses = 0;
+        this.frequency = 0;
+    }
 }
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main (String[] args) {
+        Symbol[] symbols = new Symbol[9];
+        symbols[0].symbol = '∞';
+        symbols[1].symbol = '☺';
+        symbols[2].symbol = '♀';
+        symbols[3].symbol = '♂';
+        symbols[4].symbol = '♠';
+        symbols[5].symbol = '♣';
+        symbols[6].symbol = '♥';
+        symbols[7].symbol = '♦';
+        symbols[8].symbol = '♫';
 
 
     }
